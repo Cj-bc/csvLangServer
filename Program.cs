07e4734a10f3202fe9bce3d2833a537135f94dd3
Cj-bc <cj.bc-sd@outlook.jp>
2024-10-27 22:36:43 +0900

    feat: Implement Minimum server with constant SignatureHelp capability

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using OmniSharp.Extensions.LanguageServer.Server;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+var server = await LanguageServer.From(
+    options =>
+    options
+    .WithInput(Console.OpenStandardInput())
+    .WithOutput(Console.OpenStandardOutput())
+    .WithHandler<SignatureHelpHandler>()
+);
+
+await server.WaitForExit.ConfigureAwait(false);
+
+
+internal class SignatureHelpHandler : ISignatureHelpHandler
+{
+    public SignatureHelpRegistrationOptions GetRegistrationOptions(SignatureHelpCapability capability, ClientCapabilities clientCapabilities)
+    {
+        var opts = new SignatureHelpRegistrationOptions();
+        opts.DocumentSelector = TextDocumentSelector.ForPattern("*.csv");
+        opts.TriggerCharacters = Container.From(",");
+        opts.RetriggerCharacters = Container.From(",");
+
+        return opts;
+    }
+
+    public async Task<SignatureHelp?> Handle(SignatureHelpParams param, CancellationToken cancellationToken)
+    {
+        string[] content = await File.ReadAllLinesAsync(param.TextDocument.Uri.GetFileSystemPath());
+        SignatureInformation signature = new SignatureInformation() with { Label = "Example Signature" };
+        SignatureHelp ret = new SignatureHelp() with { Signatures = Container<SignatureInformation>.From(signature) };
+        return ret;
+    }
+}
